3be61975b8b0dde5458af08f924e2fe2b0c0eb13
Tony Lee <tonyh.lee@yahoo.com>
2019-12-11 12:35:36 -0800

    added script to run validations

--- codalab_sh/test_trends.sh
+++ codalab_sh/test_trends.sh
@@ -1,5 +1,4 @@
-export STOCK_HOME=/root/stock/
-cd /root/cs221_project/baselines
+#!/bin/bash
 for (( c=1; c<=50; c++ ))
 do
    echo "Run $c times"
@@ -36,6 +35,3 @@ do
    python -m baselines.run --alg=ppo2     --env=Stock-v0 --network=mlp --num_timesteps=1e4 --play --train_start=20010101  --train_end=20180900 --test_start=20190101 --test_end=20190430
    python -m baselines.run --alg=trpo_mpi --env=Stock-v0 --network=mlp --num_timesteps=1e4 --play --train_start=20010101  --train_end=20180900 --test_start=20190101 --test_end=20190430
 done
-ls /root/cs221_project/baselines
-python extract_results.py $STOCK_HOME/results
-cat $STOCK_HOME/results/final_result.json
